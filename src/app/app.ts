f0b0944f39e0d384decc4874f815331c76c9f50b
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-02 13:31:42 -0500

    chore: add ngx-translate for internationalization support.
    - Integrate  and  for i18n.
    - Configure default and fallback languages in app.config.ts.
    - Add placeholder translation files for English and Spanish.
    - Update package.json and package-lock.json with new dependencies.
    - inject translate service in app component and configure default language.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('agrotrack-web-app');
+  private translate: TranslateService;
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
 }
