8d0bc2e6a6e2d4ef9d83ad9a095bd8286c9a7722
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-05 16:05:52 -0500

    feat(layout): enhance layout with toolbar and menu functionality

--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,26 +1,40 @@
 import { Component } from '@angular/core';
-import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageSwitcher } from '../language-switcher/language-switcher';
+import { MatSidenavContainer, MatSidenav, MatSidenavContent } from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { MatNavList, MatListItem, MatListItemIcon, MatListItemTitle } from '@angular/material/list';
+import {MatTooltip} from '@angular/material/tooltip';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
-import {MatButton} from '@angular/material/button';
-import {TranslatePipe} from '@ngx-translate/core';
-import {LanguageSwitcher} from '../language-switcher/language-switcher';
-
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-layout',
+  standalone: true,
   imports: [
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-    MatToolbar,
-    MatToolbarRow,
-    MatButton,
-    TranslatePipe,
-    LanguageSwitcher
+    // Angular
+    RouterOutlet, RouterLink, RouterLinkActive,
+    // i18n
+    TranslatePipe, LanguageSwitcher,
+    // Angular Material (standalone)
+    MatSidenavContainer, MatSidenav, MatSidenavContent, MatIcon, MatIconButton,
+    MatNavList, MatListItem, MatListItemIcon, MatListItemTitle, MatTooltip, MatToolbar, MatToolbarRow, NgOptimizedImage
   ],
   templateUrl: './layout.html',
-  styleUrl: './layout.css'
+  styleUrls: ['./layout.css']
 })
 export class Layout {
+  isMenuOpen = true;
 
+  options = [
+    { label: 'Organization',  icon: 'business',      route: '/organizations' },
+    { label: 'Tasks', icon: 'assignment_turned_in', route: '/subscriptions' },
+    { label: 'Settings',      icon: 'settings',      route: '/settings' }
+  ];
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
