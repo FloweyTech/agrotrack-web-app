1545c95a18d69172dad3379de355a1a9521a5fba
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-05 22:54:35 -0500

    feat(organization): add organization form with validation and subscription management

---
+++ src/app/organization/presentation/views/organization-form/organization-form.ts
@@ -0,0 +1,89 @@
+import { Component, inject } from '@angular/core';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { OrganizationStore } from '../../../application/organization.store';
+import { Organization, OrganizationStatus } from '../../../domain/model/organization.entity';
+import { Subscription, SubscriptionPlan, SubscriptionStatus } from '../../../domain/model/subscription.entity';
+import {MatError, MatFormField, MatLabel} from '@angular/material/form-field';
+import {TranslatePipe} from '@ngx-translate/core';
+import {MatInput} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+
+
+@Component({
+  selector: 'app-organization-form',
+  standalone: true,
+  templateUrl: './organization-form.html',
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    TranslatePipe,
+    MatLabel,
+    MatInput,
+    MatError,
+    MatButton,
+    MatSelectModule,
+    MatOptionModule
+  ],
+  styleUrls: ['./organization-form.css']
+})
+export class OrganizationForm {
+  private fb = inject(FormBuilder);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private store = inject(OrganizationStore);
+
+  form = this.fb.group({
+    name: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] })
+  });
+
+  isEdit = false;
+  organizationId: number | null = null;
+
+  constructor() {
+    this.route.params.subscribe(params => {
+      this.organizationId = params['id'] ? +params['id'] : null;
+      this.isEdit = !!this.organizationId;
+
+      if (this.isEdit && this.organizationId !== null) {
+        const organization = this.store.getOrganizationById(this.organizationId)();
+        if (organization) {
+          this.form.patchValue({ name: organization.name });
+        }
+      }
+    });
+  }
+
+  submit() {
+    if (!this.form.valid) return;
+
+    const organization = new Organization({
+      id: this.organizationId ?? Date.now(),
+      name: this.form.value.name!,
+      members: [],
+      status: OrganizationStatus.ACTIVE,
+      subscription: new Subscription({
+        id: Math.floor(Math.random() * 10000),
+        plan: SubscriptionPlan.AGROSTART,
+        startDate: new Date(),
+        endDate: new Date(new Date().setMonth(new Date().getMonth() + 1)), // +1 año
+        status: SubscriptionStatus.ACTIVE
+      })
+    });
+
+    if (this.isEdit) {
+      this.store.updateOrganization(organization);
+    } else {
+      this.store.addOrganization(organization);
+    }
+
+
+    this.router.navigate(['/organization']).then();
+  }
+  goBack(): void {
+    this.router.navigate(['/organization']).then();
+  }
+
+}
