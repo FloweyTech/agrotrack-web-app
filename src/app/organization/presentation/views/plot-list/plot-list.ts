fe1e8fa3946554e830046b7d5fa413bd5cbf4256
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-08 03:17:27 -0500

    feat(plot-list): add plot list component with create, edit, and delete functionality

---
+++ src/app/organization/presentation/views/plot-list/plot-list.ts
@@ -0,0 +1,57 @@
+import { Component, inject } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { OrganizationStore } from '../../../application/organization.store';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
+import { TitleCasePipe, DecimalPipe } from '@angular/common';
+
+@Component({
+  selector: 'app-plot-list',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    TranslatePipe,
+    TitleCasePipe,
+    DecimalPipe
+  ],
+  templateUrl: './plot-list.html',
+  styleUrls: ['./plot-list.css']
+})
+export class PlotList {
+  readonly store = inject(OrganizationStore);
+  private readonly router = inject(Router);
+  private readonly route = inject(ActivatedRoute);
+  private readonly translate = inject(TranslateService);
+
+  organizationId!: number;
+
+  constructor() {
+    this.route.params.subscribe(params => {
+      this.organizationId = +params['orgId'];
+      if (this.organizationId) {
+        this.store['loadPlots'](this.organizationId);
+      }
+    });
+  }
+
+  navigateToNew(): void {
+    this.router.navigate(['/organization', this.organizationId, 'plots', 'new']).then();
+  }
+
+  deletePlot(id: number): void {
+    const confirmMessage = this.translate.instant('plot.deleteConfirm');
+    if (confirm(confirmMessage)) {
+      this.store.deletePlot(id);
+    }
+  }
+
+  editPlot(id: number): void {
+    this.router.navigate(['/organization', this.organizationId, 'plots', id, 'edit']).then();
+  }
+}
