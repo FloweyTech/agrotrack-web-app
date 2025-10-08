a132472c339eded763ce624b3ad4aa7298bb0e05
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-08 03:47:30 -0500

    feat(organization): add routes for managing plots within organizations

--- src/app/organization/presentation/views/organization.routes.ts
+++ src/app/organization/presentation/views/organization.routes.ts
@@ -1,8 +1,18 @@
 const organizationList = () => import('./organization-list/organization-list').then(m => m.OrganizationList);
 const organizationForm = () => import('./organization-form/organization-form').then(m => m.OrganizationForm);
+const plotList = () => import('./plot-list/plot-list').then(m => m.PlotList);
+const plotForm = () => import('./plot-form/plot-form').then(m => m.PlotForm);
 
 export const organizationRoutes = [
   { path: '', loadComponent: organizationList },
   { path: 'new', loadComponent: organizationForm },
-  { path: ':id/edit', loadComponent: organizationForm }
+  { path: ':id/edit', loadComponent: organizationForm },
+  {
+    path: ':orgId/plots',
+    children: [
+      { path: '', loadComponent: plotList },
+      { path: 'new', loadComponent: plotForm },
+      { path: ':id/edit', loadComponent: plotForm }
+    ]
+  }
 ];
