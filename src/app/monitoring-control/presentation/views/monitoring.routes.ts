c6a462c42edc8d1dcf3ab0ec133b49a9ebb47f24
njlmrvllr <anjaliamarovillar@outlook.com>
2025-10-09 03:51:29 -0500

    feat(plant-registration): implement plant registration form and list with session management

--- src/app/monitoring-control/presentation/views/monitoring.routes.ts
+++ src/app/monitoring-control/presentation/views/monitoring.routes.ts
@@ -1,7 +1,9 @@
 const readingList = () => import('./reading-list/reading-list').then(m => m.ReadingList);
 const readingForm = () => import('./reading-form/reading-form').then(m => m.ReadingForm);
+const plantRegistrationList = () => import('./plant-registration-list/plant-registration-list').then(m => m.PlantRegistrationList);
+
 export const monitoringRoutes = [
   { path: '', loadComponent: readingList },
   { path: 'new', loadComponent: readingForm },
-
+  { path: 'plant-registration', loadComponent: plantRegistrationList },
 ];
