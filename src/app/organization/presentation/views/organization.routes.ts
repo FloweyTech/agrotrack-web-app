da447469e7ef47641a0144b49f07f0adbdca2ddb
diegovilcasaboya <diegovilcatut@gmail.com>
2025-10-27 04:04:08 -0500

    feature(profile): update organization routes

--- src/app/organization/presentation/views/organization.routes.ts
+++ src/app/organization/presentation/views/organization.routes.ts
@@ -2,11 +2,13 @@ const organizationList = () => import('./organization-list/organization-list').t
 const organizationForm = () => import('./organization-form/organization-form').then(m => m.OrganizationForm);
 const plotList = () => import('./plot-list/plot-list').then(m => m.PlotList);
 const plotForm = () => import('./plot-form/plot-form').then(m => m.PlotForm);
+const organizationMembers = () => import('./organization-members/organization-members').then(m => m.OrganizationMembers);
 
 export const organizationRoutes = [
   { path: '', loadComponent: organizationList },
   { path: 'new', loadComponent: organizationForm },
   { path: ':id/edit', loadComponent: organizationForm },
+  { path: ':id/members', loadComponent: organizationMembers },
   {
     path: ':orgId/plots',
     children: [
