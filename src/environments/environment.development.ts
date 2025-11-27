fcad71687bd3aa36cb6a68d4eee661daa85aac93
GoldQP <gonza1622508@gmail.com>
2025-11-26 21:06:06 -0500

    feat(weather): update weather API base URL and enhance location form styling

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  platformProviderApiBaseUrl: 'http://localhost:3000',
+  platformProviderApiBaseUrl: 'https://agrotrack-mockapi.azurewebsites.net/api/v1',
   platformProviderSubscriptionsEndpointPath: '/subscriptions',
   platformProviderOrganizationsEndpointPath: '/organizations',
   platformProviderPlotsEndpointPath: '/plots',
